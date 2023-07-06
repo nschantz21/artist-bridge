8806655a170982154ceb07c39c8547910f32672d
nschantz21 <nschantz21@gmail.com>
2023-07-06 04:19:20 +0000

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # artist-bridge
+
+Example usage:
+```{python}
+import spotipy
+from artistbridge import make_playlist
+
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+scope = "playlist-modify-public"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri="http://example.com",
+        scope=scope,
+        open_browser=False)
+    )
+
+new_playlist = sp.user_playlist_create(
+    user=SPOTIPY_USER_ID, 
+    name="cruel_summer_street_spirit", 
+    public=True)
+
+
+
+#search_results = sp.search("street spirit", type="track", limit=10)
+search_results = sp.search("cruel summer", type="track", limit=10)
+
+for i in search_results["tracks"]["items"]:
+    print(i["name"], i["id"])
+
+starting_track = Track(song_id='1BxfuPKGuaTgP7aM0Bbdwr')
+ending_track = Track(song_id="2QwObYJWyJTiozvs0RI7CF")
+
+my_playlist = make_playlist(starting_track, ending_track, 50)
+
+sp.user_playlist_add_tracks(
+    SPOTIPY_USER_ID,
+    playlist_id=new_playlist['uri'],
+    tracks=[x.id for x in my_playlist]
+)
+```
---
+++ artistbridge/__init__.py
@@ -0,0 +1,98 @@
+import spotipy
+import numpy as np
+from numpy.linalg import norm
+
+class Track: 
+    def __init__(self, data=None, song_id=None):
+        if song_id:
+            data = sp.audio_features(tracks=[song_id,])[0]
+        self.data = data
+        self.id = data['id']
+        self.vector = self.get_vector(data)
+
+    def get_vector(self, data):
+        song_vector = [
+            data.get("danceability", 0),
+            data.get("energy", 0),
+            self.clip(data.get("loudness", 0), -60, 0, 60),
+            data.get("speechiness", 0),
+            data.get("acousticness", 0),
+            data.get("instrumentalness", 0),
+            data.get("liveness", 0),
+            data.get("valence", 0),  # happiness
+            self.clip(data.get("tempo", 0), 0, 200, 200),
+        ]
+        return np.array(song_vector)
+
+    def clip(
+        self,
+        raw_value: int, 
+        min_value: int, 
+        max_value: int, 
+        standardize: int = 0) -> float:
+        """
+        
+        """
+        clipped = max(min_value, min(max_value, raw_value))
+        if min_value < 0:
+            clipped += -min_value
+        if standardize != 0:
+            clipped /= standardize
+        return clipped
+    
+    @staticmethod
+    def cmp(left, right):
+        """
+        return the cosine similarity of the vector of two tracks
+        """
+        a, b = left.vector, right.vector
+        cosine = np.dot(a,b) / (norm(a)*norm(b))
+        return cosine
+
+    def _recommendations(self):
+        recs = sp.recommendations(
+            seed_tracks=[self.id,]
+        )
+
+        return recs
+    
+    def recommendations(self):
+        recs = self._recommendations()
+        rec_ids = [r['id'] for r in recs['tracks']]
+        return rec_ids
+
+    def get_track(self):
+        return sp.track(self.id)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+
+def make_playlist(start_track: Track, end_track: Track, steps: int=10):
+    playlist_start = [start_track,]
+    playlist_end = [end_track,]
+    s = start_track
+    e = end_track
+
+    for i in range(steps//2):
+        # get recommendations
+        start_recs = [Track(song_id=x) for x in s.recommendations()]
+        end_recs = [Track(song_id=x) for x in e.recommendations()]
+        # make comparisons
+        start_end_comparisons = [Track.cmp(s, x) for x in end_recs]
+        end_start_comparisons = [Track.cmp(e, x) for x in start_recs]
+        # pick the best
+        s_index = np.argmax(end_start_comparisons)
+        s = start_recs[s_index]
+        e_index = np.argmax(start_end_comparisons)
+        e = end_recs[e_index]
+        playlist_start.append(s)
+        playlist_end.append(e)
+
+    full_playlist = playlist_start + playlist_end[::-1]
+    sorted_playlist = sorted(set(full_playlist), key=lambda x: full_playlist.index(x))
+    return sorted_playlist
